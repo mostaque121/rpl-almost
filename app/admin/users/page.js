94f98367a78279b3f851a5d14a97554c2dd25495
mostaque123 <mostaqueahmad121@gmail.com>
2024-10-03 21:39:38 +0600

    Your commit message here

--- app/admin/users/page.js
+++ app/admin/users/page.js
@@ -1,7 +1,18 @@
 import { fetchData } from "@/app/lib/fetchData";
 import UserTable from "./components/UserTable";
 export default async function Page() {
-  const users = await fetchData('api/admin/users');
+  let users = null;
+
+  try {
+    users = await fetchData('api/admin/users');
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    if (error.response?.status === 401) {
+      users = null;
+    } else {
+      users = [];
+    }
+  }
   return (users &&
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">User Management</h1>
