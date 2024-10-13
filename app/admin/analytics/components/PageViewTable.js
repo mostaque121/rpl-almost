9c0f836907bbce73e01d390fd1f4fce68d575929
mostaque123 <mostaqueahmad121@gmail.com>
2024-10-13 16:54:05 +0600

    again change style

--- app/admin/analytics/components/PageViewTable.js
+++ app/admin/analytics/components/PageViewTable.js
@@ -28,31 +28,31 @@ const PageViewTable = ({ pageViews, totalPageViews, totalEngagementTime, engagem
         <div>
             <table className="border-collapse w-full">
                 <thead>
-                    <tr className="border-b-2 border-black">
-                        <th className="py-2 text-left">Page</th>
-                        <th className="py-2 text-right">Total Views</th>
-                        <th className="py-2 text-right">Total Engage Time</th>
-                        <th className="py-2 text-right">Avg Engage Time</th>
+                    <tr className="border border-black">
+                        <th className="py-2 px-3 text-left">Page</th>
+                        <th className="py-2 px-3 text-right">Total Views</th>
+                        <th className="py-2 px-3 text-right">Total Engage Time</th>
+                        <th className="py-2 px-3 text-right">Avg Engage Time</th>
                     </tr>
                 </thead>
                 <tbody>
                     {Object.keys(pageViews).map((path, index) => (
-                        <tr key={path} className={`border-t border-b ${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}`}>
-                            <td className="py-2 pl-2">{path}</td>
-                            <td className="py-2 text-right">{pageViews[path]}</td>
-                            <td className="py-2 text-right">
+                        <tr key={path} className={`border ${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}`}>
+                            <td className="py-2 px-3 ">{path}</td>
+                            <td className="py-2 px-3 text-right">{pageViews[path]}</td>
+                            <td className="py-2 px-3 text-right">
                                 {engagementTimes[path] !== undefined ? formatTime(engagementTimes[path]) : 'N/A'}
                             </td>
-                            <td className="py-2 text-right">
+                            <td className="py-2 px-3 text-right">
                                 {engagementTimes[path] !== undefined ? getAvgEngageTime(engagementTimes[path], pageViews[path]) : 'N/A'}
                             </td>
                         </tr>
                     ))}
-                    <tr className="border-t border-b-2 border-black">
-                        <td className="py-2 font-bold">Total</td>
-                        <td className="py-2 font-bold text-right">{totalPageViews}</td>
-                        <td className="py-2 font-bold text-right">{formatTime(totalEngagementTime)}</td>
-                        <td className="py-2 font-bold text-right">{getAvgEngageTime(totalEngagementTime, totalPageViews)}</td>
+                    <tr className="border border-black">
+                        <td className="py-2 px-3 font-bold">Total</td>
+                        <td className="py-2 px-3 font-bold text-right">{totalPageViews}</td>
+                        <td className="py-2 px-3 font-bold text-right">{formatTime(totalEngagementTime)}</td>
+                        <td className="py-2 px-3 font-bold text-right">{getAvgEngageTime(totalEngagementTime, totalPageViews)}</td>
                     </tr>
                 </tbody>
             </table>
