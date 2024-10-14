05bbf32de396b5b3ecb52bd30b9146688446b17c
mostaque123 <mostaqueahmad121@gmail.com>
2024-10-14 14:30:32 +0600

    fixed the unit section

--- app/components/qualifications/Units.js
+++ app/components/qualifications/Units.js
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from 'react';
-import { MdExpandLess, MdExpandMore } from 'react-icons/md'; // Import the icons
+import { MdExpandMore } from 'react-icons/md'; // Import the icons
 
 const Units = ({ coreUnits, electiveUnits }) => {
     const [isCoreUnitsOpen, setCoreUnitsOpen] = useState(false);
@@ -10,18 +10,18 @@ const Units = ({ coreUnits, electiveUnits }) => {
         <div >
             <div className="mb-4">
                 <h2
-                    className="sm:text-xl text-base font-semibold cursor-pointer mb-2 flex items-center justify-between transition-colors duration-300 text-gray-800 hover:text-indigo-600"
+                    className="sm:text-xl text-base font-semibold cursor-pointer mb-2 flex items-center justify-between transition-colors duration-300 text-gray-800 hover:text-copy"
                     onClick={() => setCoreUnitsOpen(!isCoreUnitsOpen)}
                 >
                     <span className="flex items-center">
                         <span className={`mr-2 transition-transform duration-300 ${isCoreUnitsOpen ? 'rotate-180' : 'rotate-0'}`}>
-                            {isCoreUnitsOpen ? <MdExpandLess size={24} /> : <MdExpandMore size={24} />}
+                            <MdExpandMore size={24} />
                         </span>
                         Core Units
                     </span>
                 </h2>
                 <div
-                    className={`overflow-hidden transition-all duration-300 ease-in-out ${isCoreUnitsOpen ? 'max-h-screen' : 'max-h-0'
+                    className={`overflow-hidden transition-all duration-300 ease-in-out ${isCoreUnitsOpen ? '' : 'max-h-0'
                         }`}
                 >
                     <div className='text-sm text-gray-600 py-2' dangerouslySetInnerHTML={{ __html: coreUnits }} />
@@ -31,21 +31,21 @@ const Units = ({ coreUnits, electiveUnits }) => {
             {/* Elective Units Section */}
             <div>
                 <h2
-                    className="sm:text-xl text-base font-semibold cursor-pointer mb-2 flex items-center justify-between transition-colors duration-300 text-gray-800 hover:text-indigo-600"
+                    className="sm:text-xl text-base font-semibold cursor-pointer mb-2 flex items-center justify-between transition-colors duration-300 text-gray-800 hover:text-copy"
                     onClick={() => setElectiveUnitsOpen(!isElectiveUnitsOpen)}
                 >
                     <span className="flex items-center">
                         <span className={`mr-2 transition-transform duration-300 ${isElectiveUnitsOpen ? 'rotate-180' : 'rotate-0'}`}>
-                            {isElectiveUnitsOpen ? <MdExpandLess size={24} /> : <MdExpandMore size={24} />}
+                            <MdExpandMore size={24} />
                         </span>
                         Elective Units
                     </span>
                 </h2>
                 <div
-                    className={`overflow-hidden transition-all duration-300 ease-in-out ${isElectiveUnitsOpen ? 'max-h-screen' : 'max-h-0'
+                    className={`overflow-hidden transition-all duration-300 ease-in-out ${isElectiveUnitsOpen ? 'h-auto' : 'h-0'
                         }`}
                 >
-                    <div className='text-sm text-gray-600 py-2' dangerouslySetInnerHTML={{ __html: electiveUnits }} />
+                    <div className='text-sm h-full text-gray-600 py-2' dangerouslySetInnerHTML={{ __html: electiveUnits }} />
                 </div>
             </div>
         </div>
